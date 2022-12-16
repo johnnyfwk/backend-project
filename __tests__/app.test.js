2bdab48bc28f4dd0171c50d039cdea5b23db610e
johnnyfwk <johnnyfong@live.com>
2022-12-16 09:41:10 +0000

    Backend Project > Task 9 - Committing for review.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -454,3 +454,23 @@ describe("PATCH /api/reviews/:review_id", () => {
         })
     })
 })
+
+describe("GET /api/users", () => {
+    test("Returns a 200 status code and an array of user objects with each object containing the following properties: 'username', 'name', and 'avatar_url'.", () => {
+        return request(app)
+            .get('/api/users')
+            .expect(200)
+            .then((response) => {
+                expect(response.body.users).toHaveLength(4);
+                response.body.users.forEach((user) => {
+                    expect(user).toEqual(
+                        expect.objectContaining({
+                            username: expect.any(String),
+                            name: expect.any(String),
+                            avatar_url: expect.any(String)
+                        })
+                    )
+                })
+            })
+    })
+})
---
+++ models/users.js
@@ -0,0 +1,18 @@
+const db = require('../db/connection.js');
+
+function selectUsers() {
+    const queryString = `
+        SELECT *
+        FROM users;
+    `
+
+    return db
+        .query(queryString)
+        .then((result) => {
+            return result.rows;
+        })
+}
+
+module.exports = {
+    selectUsers
+}
